cd4b6f342785453a32a059595731a1e9bab3d5ea
Egor-Voronov <93457786+Egor-Voronov@users.noreply.github.com>
2023-01-12 22:30:36 +0300

    Add files via upload

---
+++ pythonProject1/i.js
@@ -0,0 +1,7 @@
+// let img = document.querySelectorAll('.i')
+// console.log(img.length)
+// if(img.length > 7) {
+//     for (let iter = 7; iter <= img.length; iter++) {
+//         img[iter].classList.add('none')
+//     }
+// } жиэс солюшн
